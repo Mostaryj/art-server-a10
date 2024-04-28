cf6fbeadfe5b3ca1e1a51da93dd21d7c68bad6a1
Mostary Jahan <mostaryjahan01@gmail.com>
2024-04-28 18:16:06 +0600

    added get post

--- index.js
+++ index.js
@@ -5,21 +5,15 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
-
 // console.log(process.env.DB_USER)
 // console.log(process.env.DB_PASS)
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kc8fcbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,16 +21,33 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const artCollection = client.db("artDB").collection("art");
+
+    app.get("/art", async (req, res) => {
+      const cursor = artCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/art", async (req, res) => {
+      const newArt = req.body;
+      console.log(newArt);
+      const result = await artCollection.insertOne(newArt);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    //await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -44,20 +55,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
 app.get("/", (req, res) => {
-    res.send("art server is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(` art is running on port: ${port}`);
-  });
+  res.send("art server is running");
+});
+
+app.listen(port, () => {
+  console.log(` art is running on port: ${port}`);
+});
